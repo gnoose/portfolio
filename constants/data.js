b344bed69de83a7913987e2eaef3d2e577f053f2
velopace <velopace@outlook.com>
2022-05-05 10:54:50 -0500

    replace university

--- constants/data.js
+++ constants/data.js
@@ -124,7 +124,7 @@ const userData = {
     },
     {
       title: "Graduation",
-      company: "North-Eastern University, (东北大学) Shenyang, Liaoning, China",
+      company: "North-Eastern University, Shenyang, Liaoning, China",
       year: "Sep 2009 - Jul 2013",
       companyLink: "http://english.neu.edu.cn/",
       contributions: [
